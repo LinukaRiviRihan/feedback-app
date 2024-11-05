97a77be2184c46b948dd53e2813c2ac88cd34003
Linuka Rivi Rihan <linukarivirihan@gmail.com>
2024-11-05 23:44:26 +0530

    components, props and state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackData from './data/FeedbackData';
+
 function App() {
-  return <h1>My first React App</h1>;
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete this feedback?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
 }
 
 export default App;
